68408ecb2304b6a119ec62c148283a6755a29f3e
caf-3 <00caf3@gmail.com>
2020-08-28 07:38:37 +0200

    First Commit"

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI : 'mongodb+srv://blogapp:matsolo@blogapp.qfsic.mongodb.net/blogapp?retryWrites=true&w=majority'}
+    module.exports = {mongoURI : 'mongodb+srv://AltasServer:<atltasPassWord>@blogapp.qfsic.mongodb.net/AtlasServer?retryWrites=true&w=majority'}
     
 }else{
     module.exports = {mongoURI : 'mongodb://localhost/blogApp'}
